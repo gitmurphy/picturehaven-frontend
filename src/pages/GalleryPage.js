48aab1d957dabfd532ec61fd0d8fd4285bbd5741
Peter Murphy <petermurphy5672@gmail.com>
2024-10-03 11:22:47 +0100

    Fetch and display artist information

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,11 +1,39 @@
-import React from 'react';
-
-import { Container } from "react-bootstrap";
+import {React, useState, useEffect} from 'react';
+import axios from 'axios';
+import { Container, ListGroup } from "react-bootstrap";
 
 function GalleryPage () {
+
+    const [artists, setArtists] = useState([]);
+
+    useEffect(() => {
+        // Allow for asynchronous API operations
+        async function fetchArtists() {
+            try {
+                const response = await axios.get('http://localhost:8080/api/artists');
+                console.log(response);
+                setArtists(response.data);
+            } catch(error) {
+                console.error('Error fetching artists:', error);
+                alert('Error fetching artists');
+            }
+        }
+
+        fetchArtists();
+    }, []); // Empty dependency array
+
     return(
         <Container>
             <h2>Gallery Page</h2>
+            <ListGroup>
+                {artists.map((artist, index) => (
+                    <ListGroup.Item key={index}>
+                        <h4>{artist.name}</h4>
+                        <p><strong>Bio:</strong> {artist.bio}</p>
+                        <p><strong>Nationality:</strong> {artist.nationality}</p>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </Container>
     );
 }
